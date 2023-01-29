30ef14f4d9f17d12bbff9ad24569535f0d03af88
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2023-01-29 19:30:46 +0200

    Add IPredica and Predica Model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Model.Predica;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(new Controller.Calendar().zi("2023" , "Ianuarie", "1").get("text"));
+    public static void main(String[] args) throws IOException {
+        //System.out.println(new Controller.Calendar().zi("2023" , "Ianuarie", "1").get("text"));
+        var p = new Predica("C:\\Users\\Andrei\\Desktop\\Agenda-Preot\\src\\Model\\Predici.txt");
+        p.AdaugaPredica("Andrei are gutui");
+        p.AdaugaPredica("Ana are pere");
+        p.AdaugaPredica("Marius are castraveti");
+        p.EditeazaPredica(1, "Costel are pepeni");
+        p.StergePredica(0);
     }
 }
