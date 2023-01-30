acb9835c2d8b0c79e0cfeb56f65b1a0d6309ba59
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2023-01-30 09:29:29 +0200

    Add CRUD Functionality for Predica

--- src/GUIPredica.java
+++ src/GUIPredica.java
@@ -10,7 +10,10 @@ import java.util.ArrayList;
 
 public class GUIPredica {
     private String _caleFisier = "C:\\Users\\Andrei\\Desktop\\Agenda-Preot\\src\\Model\\Predici.txt";
-    public GUIPredica() {
+    private final Predica _context;
+    public GUIPredica() throws IOException {
+
+        _context = new Predica(_caleFisier);
     }
 
     public JScrollPane Vizualizare() throws IOException {
@@ -18,16 +21,51 @@ public class GUIPredica {
     }
 
 
-    public JPanel Adaugare()
+    public JScrollPane Adaugare()
     {
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Adaugare predica"));
-        return panel;
+        JPanel panel;
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel label = new JLabel("Adauga predica");
+        JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        JButton submitButton = new JButton("Adauga");
+        panel.add(label);
+        panel.add(scrollPane);
+        panel.add(submitButton);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if(textArea.getText().length() < 28) {
+                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 28 de caractere!");
+                        return;
+                    }
+                    _context.AdaugaPredica(textArea.getText());
+                    RandeazaVizualizare(panel);
+                    panel.revalidate();
+                    panel.repaint();
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        return new JScrollPane(panel);
     }
 
     public JPanel PanelVizualizare() throws IOException {
-        var Model = new Predica(_caleFisier);
         JPanel panel = new JPanel();
+        RandeazaVizualizare(panel);
+        return panel;
+    }
+
+    public void RandeazaVizualizare(JPanel panel) throws IOException {
+        panel.removeAll();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         JPanel TitlePanel = new JPanel();
@@ -41,7 +79,7 @@ public class GUIPredica {
         title.setFont(new Font("Serif", Font.PLAIN, 36));
         TitlePanel.add(title, c);
         panel.add(TitlePanel);
-        ArrayList<String> predici = Model.CitestePredici();
+        ArrayList<String> predici = _context.CitestePredici();
         for(int i = 0 ; i < predici.size() ; i++)
         {
             JPanel row = new JPanel();
@@ -51,29 +89,33 @@ public class GUIPredica {
             c.gridy = 0;
             c.weightx = 1.0;
             c.fill = GridBagConstraints.HORIZONTAL;
-            JLabel stringLabel = new JLabel(predici.get(i));
+            JLabel stringLabel = new JLabel(predici.get(i).substring(0, 25) + "...");
             row.add(stringLabel, c);
 
             c.gridx = 1;
             c.weightx = 0.0;
             JButton editButton = new JButton("Edit");
+            int finalI = i;
             editButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    RandeazaEditare(panel , finalI);
+                    panel.revalidate();
+                    panel.repaint();
                 }
             });
             row.add(editButton, c);
             JButton deleteButton = new JButton("Delete");
             c.gridx = 2;
             row.add(deleteButton, c);
-            int finalI = i;
             deleteButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     try {
-                        Model.StergePredica(finalI);
-                        panel.repaint();
+                        _context.StergePredica(finalI);
+                        RandeazaVizualizare(panel);
                         panel.revalidate();
+                        panel.repaint();
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
@@ -86,6 +128,40 @@ public class GUIPredica {
 
             panel.add(row);
         }
-        return panel;
+    }
+
+    private void RandeazaEditare(JPanel panel, int id)
+    {
+        panel.removeAll();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel label = new JLabel("Editeaza predica");
+        JTextArea textArea = new JTextArea(_context.CitestePredica(id));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        JButton submitButton = new JButton("Salveaza");
+        panel.add(label);
+        panel.add(scrollPane);
+        panel.add(submitButton);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if(textArea.getText().length() < 28) {
+                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 28 de caractere!");
+                        return;
+                    }
+                    _context.EditeazaPredica(id, textArea.getText());
+                    RandeazaVizualizare(panel);
+                    panel.revalidate();
+                    panel.repaint();
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 }
--- src/MainGUI.java
+++ src/MainGUI.java
@@ -65,7 +65,11 @@ public class MainGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.getContentPane().remove(currentPanel);
-                currentPanel = new GUIPredica().Adaugare();
+                try {
+                    currentPanel = new GUIPredica().Adaugare();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
                 frame.getContentPane().add(BorderLayout.CENTER, currentPanel);
                 frame.getContentPane().revalidate();
                 frame.getContentPane().repaint();
