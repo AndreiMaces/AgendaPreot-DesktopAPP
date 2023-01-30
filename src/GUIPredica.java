329a5ee6202b9f40e58646c221c51ba730bfe72f
MacesAndrei <andrei.maces@cst.ro>
2023-01-30 17:05:58 +0200

    Add improvements to interface

--- src/GUIPredica.java
+++ src/GUIPredica.java
@@ -8,41 +8,73 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class GUIPredica {
+public class GUIPredica
+{
     private String _caleFisier = "C:\\Users\\Andrei\\Desktop\\Agenda-Preot\\src\\Model\\Predici.txt";
     private final Predica _context;
-    public GUIPredica() throws IOException {
+    public GUIPredica() throws IOException
+    {
 
         _context = new Predica(_caleFisier);
     }
-
-    public JScrollPane Vizualizare() throws IOException {
-        return new JScrollPane(this.PanelVizualizare());
+    public JScrollPane Vizualizare() throws IOException
+    {
+        JPanel panel = new JPanel();
+        RandeazaVizualizare(panel);
+        return new JScrollPane(panel);
     }
-
-
     public JScrollPane Adaugare()
     {
         JPanel panel;
         panel = new JPanel();
+        RandareAdaugare(panel);
+        return new JScrollPane(panel);
+    }
+    private void RandareAdaugare(JPanel panel)
+    {
+        panel.removeAll();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel label = new JLabel("Adauga predica");
+        JPanel TitlePanel = new JPanel();
+        TitlePanel.setMaximumSize(new Dimension(TitlePanel.getMaximumSize().width, 100));
+        TitlePanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JLabel title = new JLabel("Adaugare predica");
+        title.setFont(new Font("Serif", Font.PLAIN, 36));
+        TitlePanel.add(title, c);
         JTextArea textArea = new JTextArea();
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         JScrollPane scrollPane = new JScrollPane(textArea);
 
+        JPanel PanelButon = new JPanel();
+        PanelButon.setMaximumSize(new Dimension(PanelButon.getMaximumSize().width, 80));
+        PanelButon.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
         JButton submitButton = new JButton("Adauga");
-        panel.add(label);
+        PanelButon.add(submitButton, c);
+        panel.add(TitlePanel);
         panel.add(scrollPane);
-        panel.add(submitButton);
+        panel.add(PanelButon);
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if(textArea.getText().length() < 28) {
-                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 28 de caractere!");
+                    if(textArea.getText().length() < 10) {
+                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 10 de caractere!");
+                        return;
+                    }
+                    if(textArea.getText().contains("@"))
+                    {
+                        JOptionPane.showMessageDialog(null, "Predica nu poate contine caracterul @!");
                         return;
                     }
                     _context.AdaugaPredica(textArea.getText());
@@ -55,20 +87,14 @@ public class GUIPredica {
                 }
             }
         });
-        return new JScrollPane(panel);
     }
-
-    public JPanel PanelVizualizare() throws IOException {
-        JPanel panel = new JPanel();
-        RandeazaVizualizare(panel);
-        return panel;
-    }
-
-    public void RandeazaVizualizare(JPanel panel) throws IOException {
+    public void RandeazaVizualizare(JPanel panel) throws IOException
+    {
         panel.removeAll();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         JPanel TitlePanel = new JPanel();
+        TitlePanel.setMaximumSize(new Dimension(TitlePanel.getMaximumSize().width, 100));
         TitlePanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
@@ -83,13 +109,14 @@ public class GUIPredica {
         for(int i = 0 ; i < predici.size() ; i++)
         {
             JPanel row = new JPanel();
+            row.setMaximumSize(new Dimension(row.getMaximumSize().width, 50));
             row.setLayout(new GridBagLayout());
             c = new GridBagConstraints();
             c.gridx = 0;
             c.gridy = 0;
             c.weightx = 1.0;
             c.fill = GridBagConstraints.HORIZONTAL;
-            JLabel stringLabel = new JLabel(predici.get(i).substring(0, 25) + "...");
+            JLabel stringLabel = new JLabel(predici.get(i).length() > 28 ? predici.get(i).substring(0, 25) + "..." : predici.get(i));
             row.add(stringLabel, c);
 
             c.gridx = 1;
@@ -105,6 +132,7 @@ public class GUIPredica {
                 }
             });
             row.add(editButton, c);
+
             JButton deleteButton = new JButton("Delete");
             c.gridx = 2;
             row.add(deleteButton, c);
@@ -122,35 +150,90 @@ public class GUIPredica {
                 }
             });
 
+            JButton viewButton = new JButton("Vizualizeaza");
+            c.gridx = 3;
+            row.add(viewButton, c);
+            viewButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    RandeazaVizualizareId(panel, finalI);
+                    panel.revalidate();
+                    panel.repaint();
+                }
+            });
+
             Border border = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK);
             Border margin = BorderFactory.createEmptyBorder(5, 5, 5, 5);
             row.setBorder(BorderFactory.createCompoundBorder(border, margin));
-
             panel.add(row);
         }
-    }
 
+        JPanel PanelButon = new JPanel();
+        PanelButon.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JButton ButonAdaugare = new JButton("Adauga predica");
+        PanelButon.add(ButonAdaugare, c);
+        PanelButon.setMaximumSize(new Dimension(PanelButon.getMaximumSize().width, 80));
+        ButonAdaugare.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RandareAdaugare(panel);
+                panel.revalidate();
+                panel.repaint();
+            }
+        });
+        panel.add(PanelButon);
+    }
     private void RandeazaEditare(JPanel panel, int id)
     {
         panel.removeAll();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel label = new JLabel("Editeaza predica");
+        JPanel TitlePanel = new JPanel();
+        TitlePanel.setMaximumSize(new Dimension(TitlePanel.getMaximumSize().width, 100));
+        TitlePanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JLabel title = new JLabel("Editeaza predica");
+        title.setFont(new Font("Serif", Font.PLAIN, 36));
+        TitlePanel.add(title, c);
         JTextArea textArea = new JTextArea(_context.CitestePredica(id));
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         JScrollPane scrollPane = new JScrollPane(textArea);
 
-        JButton submitButton = new JButton("Salveaza");
-        panel.add(label);
+        JPanel PanelButon = new JPanel();
+        PanelButon.setMaximumSize(new Dimension(PanelButon.getMaximumSize().width, 80));
+        PanelButon.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JButton submitButton = new JButton("Salveaza predica");
+        PanelButon.add(submitButton, c);
+
+        panel.add(TitlePanel);
         panel.add(scrollPane);
-        panel.add(submitButton);
+        panel.add(PanelButon);
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if(textArea.getText().length() < 28) {
-                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 28 de caractere!");
+                    if(textArea.getText().length() < 10) {
+                        JOptionPane.showMessageDialog(null, "Predica trebuie sa aibe minim 10 de caractere!");
+                        return;
+                    }
+                    if(textArea.getText().contains("@"))
+                    {
+                        JOptionPane.showMessageDialog(null, "Predica nu poate contine caracterul @!");
                         return;
                     }
                     _context.EditeazaPredica(id, textArea.getText());
@@ -161,6 +244,42 @@ public class GUIPredica {
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
+            }
+        });
+    }
+    private void RandeazaVizualizareId(JPanel panel, int id)
+    {
+        panel.removeAll();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JTextArea textArea = new JTextArea(_context.CitestePredica(id));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        textArea.setEditable(false);
+
+        JPanel PanelButon = new JPanel();
+        PanelButon.setMaximumSize(new Dimension(PanelButon.getMaximumSize().width, 80));
+        PanelButon.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JButton submitButton = new JButton("Inapoi");
+        PanelButon.add(submitButton, c);
+        panel.add(scrollPane);
+        panel.add(PanelButon);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    RandeazaVizualizare(panel);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                panel.revalidate();
+                panel.repaint();
             }
         });
     }
